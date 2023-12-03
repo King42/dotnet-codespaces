275443dbd3e57acbbfd3e23b666800ed23dbadd8
Winston King <winston.king42@gmail.com>
2023-12-03 20:11:56 +0000

    Fixed bug with Day 1 Part 2

--- SampleApp/BackEnd/Advent2023.cs
+++ SampleApp/BackEnd/Advent2023.cs
@@ -44,20 +44,41 @@ public class Advent2023
 
         foreach (var line in input)
         {
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (Enum.TryParse(typeof(DigitWord), line.Substring(i), out object? result))
+                {
+
+                }
+            }
             var normalizedLine = line
-                .Replace("one", "1")
-                .Replace("two", "2")
-                .Replace("three", "3")
-                .Replace("four", "4")
-                .Replace("five", "5")
-                .Replace("six", "6")
-                .Replace("seven", "7")
-                .Replace("eight", "8")
-                .Replace("nine", "9");
+                .Replace("one", "one1one")
+                .Replace("two", "two2two")
+                .Replace("three", "three3three")
+                .Replace("four", "four4four")
+                .Replace("five", "five5five")
+                .Replace("six", "six6six")
+                .Replace("seven", "seven7seven")
+                .Replace("eight", "eight8eight")
+                .Replace("nine", "nine9nine");
             normalizedLine.First(c => int.TryParse(c.ToString(), out int firstDigit) && (answer += firstDigit * 10) != -1);
             normalizedLine.Reverse().First(c => int.TryParse(c.ToString(), out int firstDigit) && (answer += firstDigit) != -1);
         }
         return answer.ToString();
+    }
+
+    enum DigitWord
+    {
+        zero = 0,
+        one,
+        two,
+        three,
+        four,
+        five,
+        six,
+        seven,
+        eight,
+        nine
     }
 
     private static async Task<IEnumerable<string>> GetInputLines(int day, bool useTestData)
