3a96bf0caee7ce1e4ad423cf81ab61bbe581049e
Winston King <winston.king42@gmail.com>
2023-12-04 19:31:34 +0000

    Day 2 Part 1 (WIP)

--- SampleApp/BackEnd/Advent2023.cs
+++ SampleApp/BackEnd/Advent2023.cs
@@ -7,6 +7,8 @@ public class Advent2023
         {
             case 1:
                 return await Day1(part, input);
+            case 2:
+                return await Day2(part, input);
             default:
                 throw new NotImplementedException();
         }
@@ -23,6 +25,20 @@ public class Advent2023
         else
         {
             return await Day1Part2(input);
+        }
+    }
+
+    private static async Task<string> Day2(int part, IEnumerable<string> input)
+    {
+        var name = "Trebuchet?!";
+
+        if (part == 1)
+        {
+            return await Day2Part1(input);
+        }
+        else
+        {
+            return await Day2Part2(input);
         }
     }
 
@@ -44,13 +60,6 @@ public class Advent2023
 
         foreach (var line in input)
         {
-            for (int i = 0; i < line.Length; i++)
-            {
-                if (Enum.TryParse(typeof(DigitWord), line.Substring(i), out object? result))
-                {
-
-                }
-            }
             var normalizedLine = line
                 .Replace("one", "one1one")
                 .Replace("two", "two2two")
@@ -67,18 +76,80 @@ public class Advent2023
         return answer.ToString();
     }
 
-    enum DigitWord
+    private static async Task<string> Day2Part1(IEnumerable<string> input)
     {
-        zero = 0,
-        one,
-        two,
-        three,
-        four,
-        five,
-        six,
-        seven,
-        eight,
-        nine
+        var answer = 0;
+
+        var allPulls = new List<List<(int red, int green, int blue)>>();
+        foreach (var line in input)
+        {
+            allPulls.Add(Day2GetPullsForGame(line.Substring(line.IndexOf(':') + 1)));
+        }
+
+        int maxRed = 12, maxGreen = 13, maxBlue = 14;
+        int game = 1;
+        foreach (var pulls in allPulls)
+        {
+            int red = 0, green = 0, blue = 0;
+            foreach (var pull in pulls)
+            {
+                red = Math.Max(red, pull.red);
+                green = Math.Max(green, pull.green);
+                blue = Math.Max(blue, pull.blue);
+            }
+
+            if (red <= maxRed && green <= maxGreen && blue <= maxBlue)
+            {
+                answer += game;
+            }
+
+            game++;
+        }
+        return answer.ToString();
+    }
+
+    private static List<(int red, int green, int blue)> Day2GetPullsForGame(string line)
+    {
+        var pulls = new List<(int red, int green, int blue)>();
+
+        int red = 0, green = 0, blue = 0;
+        foreach (var strDice in line.Trim().Split(';', StringSplitOptions.RemoveEmptyEntries))
+        {
+            foreach (var strDie in strDice.Trim().Split(',', StringSplitOptions.RemoveEmptyEntries))
+            {
+                var trimmedStrDie = strDie.Trim();
+                var indexOfSpace = trimmedStrDie.IndexOf(' ');
+                var val = int.Parse(trimmedStrDie.Substring(0, indexOfSpace));
+                var strColor = trimmedStrDie.Substring(indexOfSpace + 1).Trim();
+                switch (strColor)
+                {
+                    case "red":
+                        red += val;
+                        break;
+                    case "green":
+                        green += val;
+                        break;
+                    case "blue":
+                        blue += val;
+                        break;
+                    default:
+                        throw new InvalidOperationException();
+                }
+            }
+        }
+        pulls.Add((red, green, blue));
+
+        return pulls;
+    }
+
+    private static async Task<string> Day2Part2(IEnumerable<string> input)
+    {
+        var answer = 0;
+
+        foreach (var line in input)
+        {
+        }
+        return answer.ToString();
     }
 
     private static async Task<IEnumerable<string>> GetInputLines(int day, bool useTestData)
