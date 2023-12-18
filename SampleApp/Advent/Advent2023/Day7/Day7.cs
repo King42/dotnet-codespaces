e0a98ea0cd29818f4585e04e27a5958348f39d34
Winston King <winston.king@mouser.com>
2023-12-18 00:52:48 -0600

    Day 7 Part 1

--- SampleApp/Advent/Advent2023/Day7/Day7.cs
+++ SampleApp/Advent/Advent2023/Day7/Day7.cs
@@ -18,9 +18,20 @@ public static class Day7
         }
     }
 
-    private static string Part1(List<(HandType hand, int bet)> data)
+    private static string Part1(List<(string hand, HandType handType, int bet)> data)
     {
-        throw new NotImplementedException();
+        var winnings = 0L;
+
+        int handIndex = 1;
+        for (var i = HandType.HighCard; i <= HandType.FiveOfAKind; i++)
+        {
+            foreach (var hand in data.Where(t => t.handType == i).Order())
+            {
+                winnings += handIndex++ * hand.bet;
+            }
+        }
+
+        return winnings.ToString();
     }
 
     private static string Part2((long time, long score) data)
@@ -30,23 +41,85 @@ public static class Day7
 
     private enum HandType
     {
-        Nothing,
+        HighCard = 0,
+        OnePair,
+        TwoPair,
+        ThreeOfAKind,
+        FullHouse,
+        FourOfAKind,
         FiveOfAKind
     }
 
-    private static List<(HandType hand, int bet)> GetDataPart1(string[] input)
+    private static List<(string hand, HandType handType, int bet)> GetDataPart1(string[] input)
     {
-        throw new NotImplementedException();
-        var data = new List<(HandType hand, int bet)>();
+        var data = new List<(string hand, HandType handType, int bet)>();
 
-        var timeLine = input[0];
-        var scoreLine = input[1];
-
-        var times = Parsers.ExtractNumbersFromList<int>(timeLine.Split(':')[1]);
-        var scores = Parsers.ExtractNumbersFromList<int>(scoreLine.Split(':')[1]);
-
+        foreach (var line in input)
+        {
+            var handAndBet = line.Split(' ');
+            var hand = handAndBet[0]
+                .Replace('T', 'a')
+                .Replace('J', 'b')
+                .Replace('Q', 'c')
+                .Replace('K', 'd')
+                .Replace('A', 'e');
+            var handType = DetermineHandType(hand);
+            var bet = int.Parse(handAndBet[1]);
+            data.Add((hand, handType, bet));
+        }
 
         return data;
+    }
+
+    private static HandType DetermineHandType(string hand)
+    {
+        var cards = new Dictionary<char, int>();
+        foreach (var c in hand)
+        {
+            if (!cards.TryAdd(c, 1))
+            {
+                cards[c]++;
+            }
+        }
+
+        bool hasThreeOfAKind = false, hasPair = false;
+        foreach (var card in cards)
+        {
+            switch (card.Value)
+            {
+                case 5:
+                    return HandType.FiveOfAKind;
+                case 4:
+                    return HandType.FourOfAKind;
+                case 3:
+                    hasThreeOfAKind = true;
+                    break;
+                case 2:
+                    if (hasPair)
+                    {
+                        return HandType.TwoPair;
+                    }
+                    hasPair = true;
+                    break;
+            }
+        }
+
+        if (hasThreeOfAKind && hasPair)
+        {
+            return HandType.FullHouse;
+        }
+        
+        if (hasThreeOfAKind)
+        {
+            return HandType.ThreeOfAKind;
+        }
+        
+        if (hasPair)
+        {
+            return HandType.OnePair;
+        }
+
+        return HandType.HighCard;
     }
 
     private static (long time, long score) GetDataPart2(string[] input)
